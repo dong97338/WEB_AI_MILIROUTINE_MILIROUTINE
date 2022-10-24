c4686658798d2658ab6bcd949e2d2167e9638f1a
Lee Si Woong <83443857+silverttthin@users.noreply.github.com>
2022-10-24 13:34:02 +0000

    Feat : auth_start prop추가해 인증 시작 여부를 확인, 그에 따라 내용변경

--- WEB(FE)/src/components/Element/RoutineBlock.tsx
+++ WEB(FE)/src/components/Element/RoutineBlock.tsx
@@ -10,6 +10,7 @@ export interface RoutineBlockProps {
     category?: string;
     auth_cycle?: number;
     percentage?: number;
+    auth_start?: boolean;
 }
 
 
@@ -21,15 +22,16 @@ export const RoutineBlock = (
         category = '학습',
         auth_cycle = 5,
         percentage = 30,
+        auth_start = true,
     }: RoutineBlockProps
 ) => {
-    const navigate = useNavigate()
+    const navigate = useNavigate();
     
+
     return (
         <div className="bg-gray-300 rounded-xl">
             <div className="flex px-4 pt-5 pb-3">
                 <img className="border rounded-xl border-black mb-2 w-40 h-40 object-cover bg-white-200 shadow-lg" src={thumbnail_img} />
-
                 <div className="ml-4">
                     <span className="text-sm text-gray-500">{host}</span>
                     <h4 className="text-2xl text-black font-bold mb-4 mt-2">{name}</h4>
@@ -37,13 +39,20 @@ export const RoutineBlock = (
                     <div className="flex mt-6">
                         <p>주 {auth_cycle}회 인증</p>
 
-                        <p className="ml-16">오늘 인증하면&nbsp;
-                            <span className="text-xl text-orange font-bold" >
-                                2주차 2회차 {/* 인증주차 기록 추후 개발 */}
-                            </span>
-                        </p>
+                        {auth_start ?
+                            <p className="ml-16">오늘 인증하면&nbsp;
+                                <span className="text-xl text-orange font-bold" >
+                                    2주차 2회차 {/* 인증주차 기록 추후 개발 */}
+                                </span>
+                            </p>
+                            :
+                            <p className="ml-16">루틴 시작일까지&nbsp;
+                                <span className="text-xl text-orange font-bold" >
+                                    D-5 {/* 디데이 추후 개발 */}
+                                </span>
+                            </p>
+                        }
                     </div>
-
                 </div>
 
                 <div className="flex flex-col items-center ml-20">
@@ -56,7 +65,6 @@ export const RoutineBlock = (
                         }} />
                     </div>
                 </div>
-
             </div>
         </div>
     )
