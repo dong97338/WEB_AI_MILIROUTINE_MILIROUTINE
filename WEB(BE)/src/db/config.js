25bf790107c66d384ee71ff7cc379635919c3617
Yongjun Park <yopark@student.42seoul.kr>
2022-10-27 08:37:24 +0900

    Fix: docker에 맞게 변경

--- WEB(BE)/src/db/config.js
+++ WEB(BE)/src/db/config.js
@@ -1,15 +1,16 @@
 const mysql = require('mysql');
 const path = require('path');
 
-require('dotenv').config({path:path.join(__dirname, '.env')});
+require('dotenv').config({ path: path.join(__dirname, '.env') });
 
 const db = mysql.createConnection({
-	host : process.env.DB_HOST,
-	user : process.env.DB_USER,
-	password : process.env.DB_PASSWORD,
-	port : process.env.DB_PORT,
-	database : process.env.DB_DATABASE
-})
+  host: 'database', // Container 밖에서 사용할 때는 'localhost'
+  user: 'miliroutine_developer',
+  password: '2022MySQL!@',
+  port: 3306,
+  database: 'miliroutine_db',
+  charset: 'utf8mb4_general_ci',
+});
 
 db.connect();
 
