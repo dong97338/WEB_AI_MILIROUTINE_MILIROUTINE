07e245fe5e62c28dadd33ff70297eeb8fc2f330e
Yongjun Park <yopark@student.42seoul.kr>
2022-10-26 14:04:20 +0900

    Feat: add method user_routine.getParticipantsById

--- WEB(BE)/src/controllers/routine.ctrl.js
+++ WEB(BE)/src/controllers/routine.ctrl.js
@@ -2,119 +2,118 @@ const data = require('../models/index');
 const jwt = require('../token/jwt');
 
 const token = {
-	isToken : (req, res) => {
-		if(req.headers.authorization && req.headers.authorization.split(' ')[1]){
-			return true;
-		}
+  isToken: (req, res) => {
+    if (req.headers.authorization && req.headers.authorization.split(' ')[1]) {
+      return true;
+    } else {
+      return false;
+    }
+  },
 
-		else{
-			return false;
-		}
-	},
-	
-	decode : (req, res) => {
-		if(!token.isToken(req, res)){
-			return res.status(400).json({
-				success : false,
-				isLogin : false,
-				err : '로그인을 해주세요'
-			})
-		};
-		
-		const jwtToken = req.headers.authorization.split(' ')[1];
-		const decoded = jwt.token.decode(jwtToken)
-		return decoded;
-	}
-}
+  decode: (req, res) => {
+    if (!token.isToken(req, res)) {
+      return res.status(400).json({
+        success: false,
+        isLogin: false,
+        err: '로그인을 해주세요',
+      });
+    }
+
+    const jwtToken = req.headers.authorization.split(' ')[1];
+    const decoded = jwt.token.decode(jwtToken);
+    return decoded;
+  },
+};
 
 const routine = {
-	make : async (req, res) =>{
-		const {name, category, image, auth_cycle, auth_description_list, start_date, duration} = req.body;
-		
-		if(!name){
-			res.status(400).json({
-				success : false,
-				err : '이름을 입력해주세요'
-			})
-		} else if(!category){
-			res.status(400).json({
-				success : false,
-				err : '카테고리를 입력해주세요'
-			})
-		} else if(!image){
-			res.status(400).json({
-				success : false,
-				err : '이미지를 첨부해주세요'
-			})
-		} else if(!auth_cycle){
-			res.status(400).json({
-				success : false,
-				err : '주기 횟수를 입력해주세요'
-			})
-		} else if(!auth_description_list){
-			res.status(400).json({
-				success : false,
-				err : '인증 방법을 입력해주세요'
-			})
-		} else if(!start_date){
-			res.status(400).json({
-				success : false,
-				err : '시작일을 입력해주세요'
-			})
-		} else if(!duration){
-			res.status(400).json({
-				success : false,
-				err : '진행기간을 입력해주세요'
-			})
-		}
+  make: async (req, res) => {
+    const { name, category, image, auth_cycle, auth_description_list, start_date, duration } = req.body;
 
-		const host = await data.user.get('id', token.decode(req, res).id);
-		const auth_description = JSON.stringify(auth_description_list);
-		
-		const param = [host[0].no , name, category, image, auth_cycle, auth_description, start_date, duration];
-		data.routine.add(param);
-		
-		const routine_id = await data.routine.getWithItems(host[0].no, name)
-		const type = 'join';
-		const param2 = [host[0].no, routine_id[0].no, type];
-		data.user_routine.add(param2);
-		
-		res.json({
-			success : true,
-			routine : param
-		})
-	},
-	
-	output : async (req, res) => {
-		const routineId = req.params.routineId;
-		
-		const param = await data.routine.get('id', routineId);
-		
-		res.json({
-			success : true,
-			routine_id : routineId,
-			routine : param[0]
-		})
-	},
-	
-	join : (req, res) => {
-		const decoded = token.decode(req, res);
-		try{
-			data.user_routine.add([decoded.no, req.params.routineId, 'join']);
-		}
-		catch(e){
-			res.status(400).json({
-				success : false,
-				err : String(e)
-			})
-		}
-		
-		res.json({
-			success : true
-		})
-	}
-}
+    if (!name) {
+      res.status(400).json({
+        success: false,
+        err: '이름을 입력해주세요',
+      });
+    } else if (!category) {
+      res.status(400).json({
+        success: false,
+        err: '카테고리를 입력해주세요',
+      });
+    } else if (!image) {
+      res.status(400).json({
+        success: false,
+        err: '이미지를 첨부해주세요',
+      });
+    } else if (!auth_cycle) {
+      res.status(400).json({
+        success: false,
+        err: '주기 횟수를 입력해주세요',
+      });
+    } else if (!auth_description_list) {
+      res.status(400).json({
+        success: false,
+        err: '인증 방법을 입력해주세요',
+      });
+    } else if (!start_date) {
+      res.status(400).json({
+        success: false,
+        err: '시작일을 입력해주세요',
+      });
+    } else if (!duration) {
+      res.status(400).json({
+        success: false,
+        err: '진행기간을 입력해주세요',
+      });
+    }
+
+    const host = await data.user.get('id', token.decode(req, res).id);
+    const auth_description = JSON.stringify(auth_description_list);
+
+    const param = [host[0].no, name, category, image, auth_cycle, auth_description, start_date, duration];
+    data.routine.add(param);
+
+    const routine_id = await data.routine.getWithItems(host[0].no, name);
+    const type = 'join';
+    const param2 = [host[0].no, routine_id[0].no, type];
+    data.user_routine.add(param2);
+
+    res.json({
+      success: true,
+      routine: param,
+    });
+  },
+
+  output: async (req, res) => {
+    const routineId = req.params.routineId;
+
+    const param = await data.routine.get('id', routineId);
+    const routine = param[0];
+    routine.hostName = (await data.user.get('no', routine.host))[0].nickname;
+    routine.participants = (await data.user_routine.getParticipantsById(routine.id))[0].count;
+    res.json({
+      success: true,
+      routine_id: routineId,
+      routine: routine,
+    });
+  },
+
+  join: (req, res) => {
+    const decoded = token.decode(req, res);
+    try {
+      data.user_routine.add([decoded.no, req.params.routineId, 'join']);
+    } catch (e) {
+      res.status(400).json({
+        success: false,
+        err: String(e),
+      });
+    }
+
+    res.json({
+      success: true,
+    });
+  },
+};
 
 module.exports = {
-	routine
-}
+  routine,
+};
--- WEB(BE)/src/models/index.js
+++ WEB(BE)/src/models/index.js
@@ -1,197 +1,246 @@
 const db = require('../db/config');
 
 const sql = {
-	select : (table, item) => {
-		return 'SELECT * FROM ' + table + ' WHERE ' + item + ' =?';
-	},
-	
-	insert : (table, inputs) => {
-		return 'INSERT INTO ' + table + ' ' + inputs + ' VALUES ?';
-	}
-}
+  select: (table, item) => {
+    return 'SELECT * FROM ' + table + ' WHERE ' + item + ' =?';
+  },
 
-const user =  {
-	get :  async (item, val)=>{
-		return new Promise(function(resolve,reject){
-			db.query('SELECT * FROM user WHERE ' + item + ' = ?', val, function(err, rows, fields){
-				if(err) {throw new Error(err);}
-				resolve(rows);
-			});
-		});
-	},
+  insert: (table, inputs) => {
+    return 'INSERT INTO ' + table + ' ' + inputs + ' VALUES ?';
+  },
+};
 
-	
-	add : async(values)=>{
-		db.query('INSERT INTO user (id,pw,email,nickname,salt) VALUES (?)', [values] , function(err,rows, fields){
-				if(err) {throw new Error(err);}
-			});
-	},
-	
-	update : async(field, value, id)=>{
-		db.query('UPDATE user SET ' + field + ' = ? WHERE id = ?', [value,id] , function(err,rows, fields){
-				if(err) {throw new Error(err);}
-			});
-	}
-}
+const user = {
+  get: async (item, val) => {
+    return new Promise(function (resolve, reject) {
+      db.query('SELECT * FROM user WHERE ' + item + ' = ?', val, function (err, rows, fields) {
+        if (err) {
+          throw new Error(err);
+        }
+        resolve(rows);
+      });
+    });
+  },
+
+  add: async (values) => {
+    db.query('INSERT INTO user (id,pw,email,nickname,salt) VALUES (?)', [values], function (err, rows, fields) {
+      if (err) {
+        throw new Error(err);
+      }
+    });
+  },
+
+  update: async (field, value, id) => {
+    db.query('UPDATE user SET ' + field + ' = ? WHERE id = ?', [value, id], function (err, rows, fields) {
+      if (err) {
+        throw new Error(err);
+      }
+    });
+  },
+};
 
 const user_category = {
-	get :  async(item, val)=>{
-		return new Promise(function(resolve,reject){
-			db.query('SELECT * FROM user_category WHERE ' + item + ' = ?', val, function(err, rows, fields){
-				if(err) {throw new Error(err);}
-				resolve(rows);
-			});
-		});
-	},
+  get: async (item, val) => {
+    return new Promise(function (resolve, reject) {
+      db.query('SELECT * FROM user_category WHERE ' + item + ' = ?', val, function (err, rows, fields) {
+        if (err) {
+          throw new Error(err);
+        }
+        resolve(rows);
+      });
+    });
+  },
 
-	
-	add : async(values)=>{
-		db.query('INSERT INTO user_category (user_no, category) VALUES (?)', [values] , function(err,rows, fields){
-			if(err) {throw new Error(err);}
-		});
-	},
-	
-	delete : async(item, val)=>{
-		db.query('DELETE FROM user_category WHERE ' + item + ' = ' + val + '' , function(err, result){
-				if(err) {throw new Error(err);}
-			});
-	}
-	
-}
+  add: async (values) => {
+    db.query('INSERT INTO user_category (user_no, category) VALUES (?)', [values], function (err, rows, fields) {
+      if (err) {
+        throw new Error(err);
+      }
+    });
+  },
+
+  delete: async (item, val) => {
+    db.query('DELETE FROM user_category WHERE ' + item + ' = ' + val + '', function (err, result) {
+      if (err) {
+        throw new Error(err);
+      }
+    });
+  },
+};
 
 const level_exp = {
-	get :  async(item, val)=>{
-		return new Promise(function(resolve,reject){
-			db.query('SELECT * FROM level_exp WHERE ' + item + ' = ?', val, function(err, rows, fields){
-				if(err) {throw new Error(err);}
-				resolve(rows);
-			});
-		});
-	}
-}
+  get: async (item, val) => {
+    return new Promise(function (resolve, reject) {
+      db.query('SELECT * FROM level_exp WHERE ' + item + ' = ?', val, function (err, rows, fields) {
+        if (err) {
+          throw new Error(err);
+        }
+        resolve(rows);
+      });
+    });
+  },
+};
 
-const routine =  {
-	get :  async(item, val)=>{
-		return new Promise(function(resolve,reject){
-			db.query('SELECT * FROM routine WHERE ' + item + ' = ?', val, function(err, rows, fields){
-				if(err) {throw new Error(err);}
-				resolve(rows);
-			});
-		});
-	},
-	
-	getWithItems :  async(val1, val2)=>{
-		return new Promise(function(resolve,reject){
-			db.query('SELECT * FROM routine WHERE host = ? AND name = ?', [val1, val2], function(err, rows, fields){
-				if(err) {throw err;}
-				resolve(rows);
-			});
-		});
-	},
-	
-	add : async(values)=>{
-		db.query('INSERT INTO routine (host,name,category,thumbnail_img,auth_cycle,auth_description_list,start_date,duration) VALUES (?)', [values] , function(err,rows, fields){
-				if(err) {throw new Error(err);}
-			});
-	}
-}
+const routine = {
+  get: async (item, val) => {
+    return new Promise(function (resolve, reject) {
+      db.query('SELECT * FROM routine WHERE ' + item + ' = ?', val, function (err, rows, fields) {
+        if (err) {
+          throw new Error(err);
+        }
+        resolve(rows);
+      });
+    });
+  },
 
-const user_routine =  {
-	get :  async(item, val)=>{
-		return new Promise(function(resolve,reject){
-			db.query('SELECT * FROM user_routine WHERE ' + item + ' = ?', val, function(err, rows, fields){
-				if(err) {throw new Error(err);}
-				resolve(rows);
-			});
-		});
-	},	
-	
-	getAll :  async()=>{
-		return new Promise(function(resolve,reject){
-			db.query('SELECT * FROM user_routine', function(err, rows, fields){
-				if(err) {throw new Error(err);}
-				resolve(rows);
-			});
-		});
-	},
+  getWithItems: async (val1, val2) => {
+    return new Promise(function (resolve, reject) {
+      db.query('SELECT * FROM routine WHERE host = ? AND name = ?', [val1, val2], function (err, rows, fields) {
+        if (err) {
+          throw err;
+        }
+        resolve(rows);
+      });
+    });
+  },
 
-	
-	add : async(values)=>{
-		db.query('INSERT INTO user_routine (user_no, routine_id, type) VALUES (?)', [values] , function(err,rows, fields){
-				if(err) {throw new Error(err);}
-			});
-	}
-}
+  add: async (values) => {
+    db.query(
+      'INSERT INTO routine (host,name,category,thumbnail_img,auth_cycle,auth_description_list,start_date,duration) VALUES (?)',
+      [values],
+      function (err, rows, fields) {
+        if (err) {
+          throw new Error(err);
+        }
+      }
+    );
+  },
+};
+
+const user_routine = {
+  get: async (item, val) => {
+    return new Promise(function (resolve, reject) {
+      db.query('SELECT * FROM user_routine WHERE ' + item + ' = ?', val, function (err, rows, fields) {
+        if (err) {
+          throw new Error(err);
+        }
+        resolve(rows);
+      });
+    });
+  },
+
+  getParticipantsById: async (id) => {
+    return new Promise(function (resolve, reject) {
+      db.query("SELECT COUNT(*) AS count FROM user_routine WHERE routine_id = ? AND type = 'join'", id, function (err, rows, fields) {
+        if (err) {
+          throw new Error(err);
+        }
+        resolve(rows);
+      });
+    });
+  },
+
+  getAll: async () => {
+    return new Promise(function (resolve, reject) {
+      db.query('SELECT * FROM user_routine', function (err, rows, fields) {
+        if (err) {
+          throw new Error(err);
+        }
+        resolve(rows);
+      });
+    });
+  },
+
+  add: async (values) => {
+    db.query('INSERT INTO user_routine (user_no, routine_id, type) VALUES (?)', [values], function (err, rows, fields) {
+      if (err) {
+        throw new Error(err);
+      }
+    });
+  },
+};
 
 const auth = {
-	get :  async(item, val)=>{
-		return new Promise(function(resolve,reject){
-			db.query('SELECT * FROM auth WHERE ' + item + ' = ?', val, function(err, rows, fields){
-				if(err) {throw new Error(err);}
-				resolve(rows);
-			});
-		});
-	},
+  get: async (item, val) => {
+    return new Promise(function (resolve, reject) {
+      db.query('SELECT * FROM auth WHERE ' + item + ' = ?', val, function (err, rows, fields) {
+        if (err) {
+          throw new Error(err);
+        }
+        resolve(rows);
+      });
+    });
+  },
 
-	
-	add : async(values)=>{
-		db.query('INSERT INTO auth (user_no, routine_id, week, day, date, img, text) VALUES (?)', [values] , function(err,rows, fields){
-				if(err) {throw new Error(err);}
-			});
-	}
-}
+  add: async (values) => {
+    db.query('INSERT INTO auth (user_no, routine_id, week, day, date, img, text) VALUES (?)', [values], function (err, rows, fields) {
+      if (err) {
+        throw new Error(err);
+      }
+    });
+  },
+};
 
 const goods = {
-	get :  async(item, val)=>{
-		return new Promise(function(resolve,reject){
-			db.query('SELECT * FROM goods WHERE ' + item + ' = ?', val, function(err, rows, fields){
-				if(err) {throw new Error(err);}
-				resolve(rows);
-			});
-		});
-	},
-	
-	getAll :  async()=>{
-		return new Promise(function(resolve,reject){
-			db.query('SELECT * FROM goods', function(err, rows, fields){
-				if(err) {throw new Error(err);}
-				resolve(rows);
-			});
-		});
-	},
-	 
-	add : async(values)=>{
-		db.query('INSERT INTO goods (name, description, thumbnail_img, price) VALUES (?)', [values] , function(err,rows, fields){
-				if(err) {throw new Error(err);}
-			});
-	}
-}
+  get: async (item, val) => {
+    return new Promise(function (resolve, reject) {
+      db.query('SELECT * FROM goods WHERE ' + item + ' = ?', val, function (err, rows, fields) {
+        if (err) {
+          throw new Error(err);
+        }
+        resolve(rows);
+      });
+    });
+  },
+
+  getAll: async () => {
+    return new Promise(function (resolve, reject) {
+      db.query('SELECT * FROM goods', function (err, rows, fields) {
+        if (err) {
+          throw new Error(err);
+        }
+        resolve(rows);
+      });
+    });
+  },
+
+  add: async (values) => {
+    db.query('INSERT INTO goods (name, description, thumbnail_img, price) VALUES (?)', [values], function (err, rows, fields) {
+      if (err) {
+        throw new Error(err);
+      }
+    });
+  },
+};
 
 const user_goods = {
-	get :  async(item, val)=>{
-		return new Promise(function(resolve,reject){
-			db.query('SELECT * FROM user_goods WHERE ' + item + ' = ?', val, function(err, rows, fields){
-				if(err) {throw new Error(err);}
-				resolve(rows);
-			});
-		});
-	},
+  get: async (item, val) => {
+    return new Promise(function (resolve, reject) {
+      db.query('SELECT * FROM user_goods WHERE ' + item + ' = ?', val, function (err, rows, fields) {
+        if (err) {
+          throw new Error(err);
+        }
+        resolve(rows);
+      });
+    });
+  },
 
-	
-	add : async(values)=>{
-		db.query('INSERT INTO user_goods (user_no, goods_id, datetime) VALUES (?)', [values] , function(err,rows, fields){
-				if(err) {throw new Error(err);}
-			});
-	}
-}
+  add: async (values) => {
+    db.query('INSERT INTO user_goods (user_no, goods_id, datetime) VALUES (?)', [values], function (err, rows, fields) {
+      if (err) {
+        throw new Error(err);
+      }
+    });
+  },
+};
 
 module.exports = {
-	user,
-	user_category,
-	level_exp,
-	routine,
-	user_routine,
-	auth,
-	goods,
-	user_goods
+  user,
+  user_category,
+  level_exp,
+  routine,
+  user_routine,
+  auth,
+  goods,
+  user_goods,
 };
