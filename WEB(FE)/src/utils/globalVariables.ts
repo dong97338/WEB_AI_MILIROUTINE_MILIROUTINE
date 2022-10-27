90d3dd3098b9e8b4fe1fe297a7d0e28f100e3f2c
Yongjun Park <yopark@student.42seoul.kr>
2022-10-27 21:24:24 +0900

    minor changes

--- WEB(FE)/src/utils/globalVariables.ts
+++ WEB(FE)/src/utils/globalVariables.ts
@@ -1,2 +1,2 @@
 export const SERVER_URL: string = 'http://localhost:3000';
-export const IMAGE_SERVER_URL: string = 'http://52.79.92.84:52187';
+export const IMAGE_SERVER_URL: string = 'http://52.79.92.84:56635/';
