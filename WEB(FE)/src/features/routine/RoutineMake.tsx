63f3f90c6379c78e68307c94eac7f2d521d08e1e
Lee Si Woong <83443857+silverttthin@users.noreply.github.com>
2022-10-28 14:36:00 +0000

    Feat : (중간커밋) 이미지 업로드 기능 구현

--- WEB(FE)/src/features/routine/RoutineMake.tsx
+++ WEB(FE)/src/features/routine/RoutineMake.tsx
@@ -1,5 +1,47 @@
-import { useState, useCallback, ChangeEvent } from "react";
-import { Form, Select, DropdownItem } from "@/components/Element";
+import React, { useState, useCallback, ChangeEvent } from "react";
+import { Form, Select, DropdownItem, Label } from "@/components/Element";
+
+
+const UploadAndDisplayImage = () => {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  return ( 
+    <div>
+      {selectedImage &&
+        (<div>
+          <img alt="Thumbnail" src={URL.createObjectURL(selectedImage)} />
+          <div className="flex justify-end">
+            <button onClick={() => setSelectedImage(null)}>이미지 삭제</button>
+          </div>
+        </div>)
+      }
+
+      <br />
+      <br />
+
+      <div>
+        <form method="post" encType="multipart/form-data">
+          <div className="flex justify-between">
+            <p className="text-sm pr-1">루틴 대표 이미지를 업로드해주세요</p>
+            <label htmlFor="ThumbnailUpload" className="cursor-pointer">
+              <Label label="업로드" />
+            </label>
+          </div>
+          <input
+            id="ThumbnailUpload"
+            className="invisible"
+            type="file"
+            name="ThumbnailUpload"
+            onChange={(e) => {
+              setSelectedImage((e.target as HTMLElement).files[0]);
+            }}
+          />
+        </form>
+      </div>
+    </div>
+  );
+};
+
 
 export const RoutineMakePage = () => {
   const [categories, setCategories] = useState([
@@ -25,32 +67,51 @@ export const RoutineMakePage = () => {
     setTitle(e.target.value);
   }, []);
 
-  return (
-    <>
-      <div className="flex flex-col items-center">
-        <div className="flex flex-col items-center">
-          <h2 className="text-black font-bold text-4xl py-6 mt-10">
-            밀리루틴 개설하기
-          </h2>
-        </div>
 
-        <div className="container max-w-xs mb-24">
-          <Form
-            label="밀리루틴 이름"
-            placeholder="10자 이내로 입력해주세요"
-            type="text"
-            value={title}
-            onChange={onChangeTitle}
-          />
-          <Select
-            label="밀리루틴 카테고리"
-            items={categories}
-            value={category}
-          />
-          <Select label="인증 주기" items={intervals} value={interval} />
-          <Select label="루틴 진행 기간" items={periods} value={period} />
-        </div>
+  return (
+
+    <div className="flex flex-col items-center">
+      <div className="flex flex-col items-center">
+        <h2 className="text-black font-bold text-4xl py-6 mt-10">
+          밀리루틴 개설하기
+        </h2>
       </div>
-    </>
+
+      <div className="container max-w-xs mb-24">
+        <Form
+          label="밀리루틴 이름"
+          placeholder="10자 이내로 입력해주세요"
+          type="text"
+          value={title}
+          onChange={onChangeTitle}
+        />
+        <Select
+          label="밀리루틴 카테고리"
+          items={categories}
+          value={category}
+        />
+        <Select label="인증 주기" items={intervals} value={interval} />
+        <Select label="루틴 진행 기간" items={periods} value={period} />
+
+        {/* <div>
+          <form method="post" encType="multipart/form-data">
+            <div className="flex justify-between">
+              <p className="text-sm pr-1">루틴 대표 이미지를 업로드해주세요</p>
+              <label htmlFor="ThumbnailUpload" className="cursor-pointer">
+                <Label label="업로드" />
+              </label>
+            </div>
+            <input className="invisible" type="file" id="ThumbnailUpload" name="ThumbnailUpload" accept="image/*" />
+          </form>
+        </div> */}
+
+        <UploadAndDisplayImage />
+
+
+      </div>
+    </div>
+
   );
 };
+
+
