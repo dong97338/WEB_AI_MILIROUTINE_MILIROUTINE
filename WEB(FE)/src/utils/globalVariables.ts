a2e4f33e18e810df4af945729d6e2097935baa47
Yongjun Park <yopark@student.42seoul.kr>
2022-10-29 17:25:37 +0900

    Fix: docker-compose에 맞게 변경

--- WEB(FE)/src/utils/globalVariables.ts
+++ WEB(FE)/src/utils/globalVariables.ts
@@ -1,3 +1,3 @@
-export const SERVER_URL: string = 'http://localhost:3000';
-export const IMAGE_SERVER_URL: string = 'http://52.79.92.84:56635/';
+export const SERVER_URL: string = 'http://backend:3000';
+export const IMAGE_SERVER_URL: string = '';
 export const WEEKDAY = ['일', '월', '화', '수', '목', '금', '토'];
