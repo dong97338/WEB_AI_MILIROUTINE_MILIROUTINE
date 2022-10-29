5bed8f3ed4d7d121224d993ee3e690f29272d5e9
Lee Si Woong <83443857+silverttthin@users.noreply.github.com>
2022-10-29 08:28:43 +0000

    Feat : 단계별 루틴 인증 방법UI 및 인증법 데이터들을 담을 배열 구현

--- WEB(FE)/src/features/routine/RoutineMake.tsx
+++ WEB(FE)/src/features/routine/RoutineMake.tsx
@@ -1,46 +1,5 @@
 import React, { useState, useCallback, ChangeEvent } from "react";
-import { Form, Select, DropdownItem, Label } from "@/components/Element";
-
-
-const UploadAndDisplayImage = () => {
-  const [selectedImage, setSelectedImage] = useState(null);
-
-  return ( 
-    <div>
-      {selectedImage &&
-        (<div>
-          <img alt="Thumbnail" src={URL.createObjectURL(selectedImage)} />
-          <div className="flex justify-end">
-            <button onClick={() => setSelectedImage(null)}>이미지 삭제</button>
-          </div>
-        </div>)
-      }
-
-      <br />
-      <br />
-
-      <div>
-        <form method="post" encType="multipart/form-data">
-          <div className="flex justify-between">
-            <p className="text-sm pr-1">루틴 대표 이미지를 업로드해주세요</p>
-            <label htmlFor="ThumbnailUpload" className="cursor-pointer">
-              <Label label="업로드" />
-            </label>
-          </div>
-          <input
-            id="ThumbnailUpload"
-            className="invisible"
-            type="file"
-            name="ThumbnailUpload"
-            onChange={(e) => {
-              setSelectedImage((e.target as HTMLElement).files[0]);
-            }}
-          />
-        </form>
-      </div>
-    </div>
-  );
-};
+import { Form, Select, DropdownItem, Label, Button } from "@/components/Element";
 
 
 export const RoutineMakePage = () => {
@@ -61,6 +20,10 @@ export const RoutineMakePage = () => {
   const [category, setCategory] = useState<DropdownItem | undefined>();
   const [interval, setInterval] = useState<DropdownItem | undefined>();
   const [period, setPeriod] = useState<DropdownItem | undefined>();
+  const [uploadedImage, setUploadedImage] = useState(null);
+  const [authWays, setAuthWays] = useState<String[]>(['']);
+  const [tmpInput, setTmpInput] = useState('');
+
 
   const onChangeTitle = useCallback((e: ChangeEvent<HTMLInputElement>) => {
     e.persist();
@@ -69,7 +32,6 @@ export const RoutineMakePage = () => {
 
 
   return (
-
     <div className="flex flex-col items-center">
       <div className="flex flex-col items-center">
         <h2 className="text-black font-bold text-4xl py-6 mt-10">
@@ -93,22 +55,85 @@ export const RoutineMakePage = () => {
         <Select label="인증 주기" items={intervals} value={interval} />
         <Select label="루틴 진행 기간" items={periods} value={period} />
 
-        {/* <div>
-          <form method="post" encType="multipart/form-data">
-            <div className="flex justify-between">
-              <p className="text-sm pr-1">루틴 대표 이미지를 업로드해주세요</p>
-              <label htmlFor="ThumbnailUpload" className="cursor-pointer">
-                <Label label="업로드" />
-              </label>
-            </div>
-            <input className="invisible" type="file" id="ThumbnailUpload" name="ThumbnailUpload" accept="image/*" />
-          </form>
-        </div> */}
 
-        <UploadAndDisplayImage />
+        <div>
+          {uploadedImage &&
+            (<div>
+              <img alt="Thumbnail" src={URL.createObjectURL(uploadedImage)} />
+              <div className="flex justify-end">
+                <button onClick={() => setUploadedImage(null)}>이미지 삭제</button>
+              </div>
+            </div>)
+          }
 
+          <br />
+          <br />
 
+          <div>
+            <form method="post" encType="multipart/form-data">
+              <div className="flex justify-between">
+                <p className="text-sm pr-1">루틴 대표 이미지를 업로드해주세요</p>
+                <label htmlFor="ThumbnailUpload" className="cursor-pointer">
+                  <Label label="업로드" />
+                </label>
+              </div>
+              <input
+                id="ThumbnailUpload"
+                className="invisible"
+                type="file"
+                name="ThumbnailUpload"
+                onChange={(e) => {
+                  setUploadedImage((e.target as HTMLElement).files[0]);
+                }}
+              />
+            </form>
+          </div>
+        </div>
       </div>
+
+
+      <div className="flex justify-between mb-10">
+        <p className="pr-52">참여 및 인증 방법을 설명해주세요</p>
+        <button className="text-sm" onClick={() => {
+          setAuthWays(prev => [...prev, tmpInput]);
+        }}>단계 추가</button>
+      </div>
+
+
+      <div>
+        {authWays.map((authWay, idx)=>{
+          let tmp = idx + 1 + "단계";
+
+          return(
+            <div key={idx} className="flex mb-20 ">
+              <Label label={tmp} margin="mr-2" />
+
+              <input className="border-x-0 border-t-0 border-b-2 focus:outline-none" onChange={(e)=>{
+                setTmpInput(e.target.value);
+                let cpyAuthWay = [...authWays];
+                cpyAuthWay[idx] = tmpInput;
+
+                setAuthWays(cpyAuthWay);
+              }}/> 
+            </div>
+          )
+        })}
+      </div>
+
+      {/* authWay 배열에는 사용자가 기술한 각 인증 방법들이 담겨있음.*/}
+
+
+
+      <div className="flex flex-col justify-center mt-20">
+        <p className="text-[14px]">· 포인트는 밀리루틴의 난이도, 인증 방법의 신뢰성을 고려하여 운영 측에서 산정할 예정입니다.</p>
+        <p className="text-[14px] mb-6">· 개설 신청 후 48시간 이내에 결과를 메일과 알림으로 알려드려요.</p>
+      </div>
+
+
+      <Button label="개설 신청하기" margin="mb-20" text="text-xl" onClick={() => {
+        // 개설 신청
+        console.log(authWays);
+      }} />
     </div>
 
   );
