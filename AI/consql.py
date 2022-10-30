62cb85322fc0a9a9340124b0423d7188c1c5708f
dong97338 <32699584+dong97338@users.noreply.github.com>
2022-10-30 12:58:47 +0000

    Feat: 로그인 한 번에 쿼리 여러개 쓸 수 있도록 클래스 작성

--- AI/consql.py
+++ AI/consql.py
@@ -1,18 +1,21 @@
 import pymysql
 import a5t  # sql 아이디 비밀번호(코드스페이스에만, 커밋x)
 
-host="database"
-user="miliroutine_developer"
-password="2022MySQL!@"
-port=3306
-database="miliroutine_db"
-use_unicode=True
-charset='utf8'
-
-def ex(q):
-    con=pymysql.connect(host=host, user=a5t.user, passwd=a5t.password, db=database, port=port, use_unicode=True, charset='utf8')
-    cur=con.cursor()
-    cur.execute(q)
-    ret=cur.fetchall()
-    con.close()
-    return ret
+class ex:
+    def __init__(self):
+        self.con=pymysql.connect(
+            host="52.79.92.84",
+            user=a5t.user,
+            password=a5t.password,
+            port=53609,
+            database="miliroutine_db",
+            use_unicode=True,
+            charset='utf8')
+        self.cur=self.con.cursor()
+    def __enter__(self):
+        return self
+    def q(self,q):
+        self.cur.execute(q)
+        return self.cur.fetchall()
+    def __exit__(self,*args):
+        self.con.close()
