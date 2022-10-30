9935a4add11568dc40eb38e7071110aa2e1f3175
김민찬 <cmk0487@naver.com>
2022-10-30 14:11:42 +0000

    home.ctrl 코드 정리

--- WEB(BE)/src/controllers/home.ctrl.js
+++ WEB(BE)/src/controllers/home.ctrl.js
@@ -91,6 +91,94 @@ const token = {
   },
 };
 
+const routine = {
+	getRecommendRoutine : async (res, refresh, userNo) =>{
+		if(!refresh){
+			var recommendNo = await ai.recommendRoutine(userNo, res);
+		}
+		else{
+			var recommendNo = await ai.recommendRefresh(userNo, refresh, res);
+		}
+		
+		recommendNo = recommendNo.substr(1, recommendNo.length - 3);
+		recommendNo = recommendNo.split(',');
+
+		for (var i = 0; i < recommendNo.length; ++i) {
+		  recommendNo[i] = Number(recommendNo[i]);
+		}
+		
+		var recommendRoutine = [];
+		for (const no of recommendNo) {
+		  const aiRoutine = await data.routine.get('id', no);
+		  const hostName = await data.user.get('no', aiRoutine[0].host);
+		  aiRoutine[0].hostName = hostName[0].nickname;
+		  aiRoutine[0].participants = await getParticipants(no);
+
+		  recommendRoutine.push(aiRoutine[0]);
+		}
+		
+		return recommendRoutine;
+	},
+	
+	getRandomRoutine : async () => {
+	  const randomRoutine = await data.routine.getRandom(NUMOFRECOMMEND);
+
+      for (const item of randomRoutine) {
+        item.hostName = await getUserName(item.host);
+        item.participants = await getParticipants(item.id);
+      }
+		
+	  return randomRoutine;
+	},
+	
+	getCurrentRoutine : async (userNo) => {
+		const authRoutine = await data.auth.getOrderByDate('user_no', userNo, NUMOFJOINED);
+
+		var currentRoutines = [];
+		var routineIdList = [];
+		var isId;
+		for (const item of authRoutine) {
+		  isId = false;
+		  for(const id of routineIdList){
+			  if(item.routine_id === id){
+				  isId = true;
+				  break;
+			  }
+		  }
+
+		  if(isId){
+			  continue;
+		  }
+
+		  const currentRoutine = await data.routine.get('id', item.routine_id);
+		  routineIdList.push(item.routine_id);
+		  currentRoutines.push(currentRoutine[0]);
+		}
+		
+		return currentRoutines;
+	},
+	
+	getRankedRoutine : async (res, JoinedRoutine) => {
+		var rankedRoutine = [];
+
+		for (var rank = MINRANK; rank <= MAXRANK; ++rank) {
+		  const routine = await data.routine.get('id', JoinedRoutine[rank - 1][0]);
+		  routine[0].participants = JoinedRoutine[rank - 1][1];
+
+		  const userInfo = await data.user.get('no', routine[0].host);
+		  if(userInfo.length === 0){
+			  return res.status(400).json({
+				success : false,
+				err : '해당 아이디의 host가 없습니다'
+			})
+		  }
+		  routine[0].hostName = userInfo[0].nickname;
+		  rankedRoutine.push(routine[0]);
+		}
+		return rankedRoutine;
+	}
+}
+
 const output = {
   // @route GET /
   home: async (req, res) => {
@@ -104,30 +192,10 @@ const output = {
     const JoinedRoutine = popular.process.sortRank(userRoutines);
 
     // 인기 루틴
-    var rankedRoutine = [];
-
-    for (var rank = MINRANK; rank <= MAXRANK; ++rank) {
-      const routine = await data.routine.get('id', JoinedRoutine[rank - 1][0]);
-      routine[0].participants = JoinedRoutine[rank - 1][1];
-		
-      const userInfo = await data.user.get('no', routine[0].host);
-	  if(userInfo.length === 0){
-		  return res.status(400).json({
-			success : false,
-			err : '해당 아이디의 host가 없습니다'
-		})
-	  }
-      routine[0].hostName = userInfo[0].nickname;
-      rankedRoutine.push(routine[0]);
-    }
+	const rankedRoutine = await routine.getRankedRoutine(res, JoinedRoutine);
 
     if (!token.isToken(req, res)) {
-      const randomRoutine = await data.routine.getRandom(NUMOFRECOMMEND);
-
-      for (const item of randomRoutine) {
-        item.hostName = await getUserName(item.host);
-        item.participants = await getParticipants(item.id);
-      }
+      const randomRoutine = await routine.getRandomRoutine();
 
       return res.json({
         success: true,
@@ -142,51 +210,10 @@ const output = {
     const userInfo = await data.user.get('id', decoded.id);
 
     // 추천 routine
-    if (!req.query.refresh) {
-      var recommendNo = await ai.recommendRoutine(decoded.no, res);
-    } else {
-      var recommendNo = await ai.recommendRefresh(decoded.no, req.query.refresh, res);
-    }
-
-    recommendNo = recommendNo.substr(1, recommendNo.length - 3);
-    recommendNo = recommendNo.split(',');
-
-    for (var i = 0; i < recommendNo.length; ++i) {
-      recommendNo[i] = Number(recommendNo[i]);
-    }
-
-    var recommendRoutine = [];
-    for (const no of recommendNo) {
-      const aiRoutine = await data.routine.get('id', no);
-      const hostName = await data.user.get('no', aiRoutine[0].host);
-      aiRoutine[0].hostName = hostName[0].nickname;
-      aiRoutine[0].participants = await getParticipants(no);
-
-      recommendRoutine.push(aiRoutine[0]);
-    }
-
-    const authRoutine = await data.auth.getOrderByDate('user_no', decoded.no, NUMOFJOINED);
-
-    var currentRoutines = [];
-	var routineIdList = [];
-	var isId;
-    for (const item of authRoutine) {
-	  isId = false;
-	  for(const id of routineIdList){
-		  if(item.routine_id === id){
-			  isId = true;
-			  break;
-		  }
-	  }
-		
-	  if(isId){
-		  continue;
-	  }
-		
-      const currentRoutine = await data.routine.get('id', item.routine_id);
-	  routineIdList.push(item.routine_id);
-      currentRoutines.push(currentRoutine[0]);
-    }
+	const recommendRoutine = await routine.getRecommendRoutine(req, res, req.query.refresh, decoded.no);
+   
+	// 참여한 루틴
+    const currentRoutines = await routine.getCurrentRoutine(decoded.no);
 
     res.json({
       success: true,
